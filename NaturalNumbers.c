428002f46ba05033882de5cc4ff24da183f413d6
Priya3690 <3gpriya5@gmail.com>
2019-01-26 07:57:23 +0000

    NaturalNumbers

---
+++ NaturalNumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int Counter=1, Count;
+	printf("How many natural numbers you want to print?");
+	scanf("%d", &Count);
+	printf("The natural numbers up to %d are", Count);
+	while (Counter <= Count) {
+		printf (" %d", Counter);
+		Counter = Counter + 1;
+	}
+	printf (".");
+}
