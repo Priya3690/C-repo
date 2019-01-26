65acf1d0615347db22786f5d8d2ab9e08c9c8afa
Priya GPSP <31795596+Priya3690@users.noreply.github.com>
2019-01-26 14:35:08 +0530

    Update NaturalNumbers.c

--- NaturalNumbers.c
+++ NaturalNumbers.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main() {
-	int Counter=1, Count;
+	int Counter= 1, Count;
 	printf("How many natural numbers you want to print?");
 	scanf("%d", &Count);
 	printf("The natural numbers up to %d are", Count);
